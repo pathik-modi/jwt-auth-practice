292d82ef7a0517cd9c6a8d6906b15d887f7ca551
Pathik Modi <modipathik95@gmail.com>
2023-11-22 14:09:47 +1300

    add steps upto 6

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -2,8 +2,8 @@
 import { useAuth0 } from '@auth0/auth0-react'
 const useIsAuthenticated = () => {
   // TODO: call the useAuth0 hook, destructure and return isAuthenticated
-  return useAuth0().isAuthenticated
-  // return true
+  const { isAuthenticated } = useAuth0()
+  return isAuthenticated
 }
 interface Props {
   children: React.ReactNode
